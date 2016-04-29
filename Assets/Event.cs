0f92dc99d766fb10649b1ff016a99b2dc054369b
Kenny Nguyen <kenimorri@gmail.com>
2016-04-29 14:48:48 -0400

    Added Restart button, a few models to work with

---
+++ Assets/Event.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Event : MonoBehaviour {
+	private bool GameOver = false;
+	private bool restart = false;
+	public Text gameOverText;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			}
+		}
+	}
+	public void gameover() {
+		gameOverText.text = "Game Over! \n Press R to restart";
+		restart = true;
+	}
+}
---
+++ Assets/Scripts/SimpleDestroyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleDestroyScript : MonoBehaviour {
+	private Event gameController;
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		gameController = gameControllerObject.GetComponent<Event> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		gameController.gameover ();
+		Destroy (other.gameObject);
+		Destroy (this.gameObject);
+	}
+}
